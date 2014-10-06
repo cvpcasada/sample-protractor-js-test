6c06bb6b89cb55222ec79eb007ee977ebd18fd18
Cyrus Venn Casada <cvpcasada@outlook.com>
2014-10-06 15:43:28 +0800

    Refactored login scenario extracted login to func.

--- tests/workbench/loginSpec.js
+++ tests/workbench/loginSpec.js
@@ -7,6 +7,31 @@ var expect = chai.expect;
 
 describe ('BMS Workbench Acceptance Tests',function() {
 
+  // perform BMS login
+  function login(username,password) {
+    var usernameTxt = dv.findElement(by.id('vaadin-username-txt'));
+
+    var passwordTxt = dv.findElement(by.id('vaadin-password-txt'));
+    var loginBtn = dv.findElement(by.id('vaadin-login-btn'));
+
+    usernameTxt.clear();
+    usernameTxt.sendKeys(username);
+
+    passwordTxt.clear();
+    passwordTxt.sendKeys(password);
+    loginBtn.click();
+
+    // waits until vaadin-home-lbl is present
+    dv.wait(function() {
+         return dv.isElementPresent(by.id('vaadin-home-lbl'));
+    },10000);
+
+    var dashboardTitle = dv.findElement(by.id('vaadin-home-lbl')).getText();
+
+    return dashboardTitle;
+  }
+
+
   beforeEach(function() {
     isAngularSite(false);
 
@@ -16,43 +41,21 @@ describe ('BMS Workbench Acceptance Tests',function() {
   });
 
   describe('BMS Login scenario',function() {
-    // all constant vars used in this scenario here
-    var executionChain = null;
-
-
     var username = 'okarin';
     var password = 'a';
 
     it ('Workbench is loaded in the browser',function() {
-      executionChain = dv.getTitle();
-
-      expect(executionChain).to.eventually.equal('Breeding Management System | Workbench');
+      expect(dv.getTitle()).to.eventually.equal('Breeding Management System | Workbench');
     });
 
     // remember all selenium driver calls are async but flow is controlled via control_flow system
     // see: https://code.google.com/p/selenium/wiki/WebDriverJs#Control_Flows
 
     it ('Will login using username = ' + username + ', password = ' + password,function() {
-        var usernameTxt = dv.findElement(by.id('vaadin-username-txt'));
-        var passwordTxt = dv.findElement(by.id('vaadin-password-txt'));
-        var loginBtn = dv.findElement(by.id('vaadin-login-btn'));
+        var dashboardTitle = login(username,password);
 
-        usernameTxt.clear();
-        usernameTxt.sendKeys(username);
-
-        passwordTxt.clear();
-        passwordTxt.sendKeys(password);
-        loginBtn.click();
-
-        // waits until vaadin-home-lbl is present
-        dv.wait(function() {
-             return dv.isElementPresent(by.id('vaadin-home-lbl'));
-        },10000);
-
-        var dashboardTitle = dv.findElement(by.id('vaadin-home-lbl')).getText();
         expect(dashboardTitle).to.eventually.equal("HOME");
     });
-
 
   });
 
